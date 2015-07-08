1ae2908441c1d576ceb64f62973996ae5dc90425
James Hugard <jhugard@blizzard.com>
2015-07-08 10:03:00 -0700

    Implement Week 1 Homework

--- week1/HW01.hs
+++ week1/HW01.hs
@@ -1,39 +1,56 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW01 where
 
+import Data.Char
+
 -- Exercise 1 -----------------------------------------
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit = undefined
+lastDigit n = n `mod` 10
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
-dropLastDigit = undefined
+dropLastDigit n = n `div` 10
 
 -- Exercise 2 -----------------------------------------
 
 toRevDigits :: Integer -> [Integer]
-toRevDigits = undefined
+-- toRevDigits n = reverse $ toDigits n
+toRevDigits n
+	| n <= 0		= []
+ 	| otherwise = lastDigit n : toRevDigits (dropLastDigit n)
+
+toDigits :: Integer -> [Integer]
+toDigits = reverse . toRevDigits
+
+toDigits' :: Integer -> [Integer]
+toDigits' n
+	| n <= 0		= []
+	| otherwise	= map (toInteger . digitToInt) $ show n
+
 
 -- Exercise 3 -----------------------------------------
 
 -- Double every second number in a list starting on the left.
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther [] 					=	[]
+doubleEveryOther [a] 					= [a]
+doubleEveryOther (a : b : tl) =	a : (b+b) : doubleEveryOther tl
 
 -- Exercise 4 -----------------------------------------
 
 -- Calculate the sum of all the digits in every Integer.
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
-
+sumDigits = foldr ((+) . sum . toDigits) 0
 
 -- Exercise 5 -----------------------------------------
 
 -- Validate a credit card number using the above functions.
 luhn :: Integer -> Bool
-luhn = undefined
+luhn n =
+		((sumDigits . doubleEveryOther . toRevDigits) n `mod` 10)
+		== 0
 
 -- Exercise 6 -----------------------------------------
 
@@ -42,4 +59,12 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 _ _ _ = []
+hanoi 1 a _ c = [(a,c)]
+-- hanoi 2 a b c = [(a,b), (a,c), (b,c)]
+hanoi n a b c =
+	concat [
+		hanoi (n-1) a c b,
+    hanoi    1  a b c,
+		hanoi (n-1) b a c
+		]
--- week1/HW01Tests.hs
+++ week1/HW01Tests.hs
@@ -24,28 +24,89 @@ ex1Tests = [ Test "lastDigit test" testLastDigit
 
 -- Exercise 2 -----------------------------------------
 
+testRevDigits :: (Integer, [Integer]) -> Bool
+testRevDigits (n, ns) = toRevDigits n == ns
+
+testDigits :: (Integer, [Integer]) -> Bool
+testDigits (n, ns) = toDigits n == ns
+
 ex2Tests :: [Test]
-ex2Tests = []
+ex2Tests =
+	[ Test "testRevDigits" testRevDigits
+		[ (1234,[4,3,2,1]),
+		  (0,[]),
+		  (-17,[])
+		],
+    Test "testDigits" testDigits
+  		[ (1234,[1,2,3,4]),
+  		  (0,[]),
+  		  (-17,[])
+  		]
+	]
 
 -- Exercise 3 -----------------------------------------
 
+testDoubleEveryOther :: ([Integer],[Integer]) -> Bool
+testDoubleEveryOther (ns, ds) = doubleEveryOther ns == ds
+
 ex3Tests :: [Test]
-ex3Tests = []
+ex3Tests =
+  [ Test "testDoubleEveryOther" testDoubleEveryOther
+    [ ([4, 9, 5, 5], [4, 18, 5, 10]),
+      ([0, 0], [0, 0])
+    ]
+  ]
 
 -- Exercise 4 -----------------------------------------
 
+testSumDigits :: ([Integer], Integer) -> Bool
+testSumDigits (ns,s) = sumDigits ns == s
+
 ex4Tests :: [Test]
-ex4Tests = []
+ex4Tests =
+  [
+    Test "testSumDigits" testSumDigits
+    [
+      ([10, 5, 18, 4], 19)
+    ]
+  ]
 
 -- Exercise 5 -----------------------------------------
 
+testLuhn :: (Integer, Bool) -> Bool
+testLuhn (n,isValid) = luhn n == isValid
+
 ex5Tests :: [Test]
-ex5Tests = []
+ex5Tests =
+  [
+    Test "testLuhn" testLuhn
+    [
+      (5594589764218858, True),
+      (1234567898765432, False)
+    ]
+  ]
 
 -- Exercise 6 -----------------------------------------
 
+testHanoi :: ( Integer, Peg, Peg, Peg, [Move] ) -> Bool
+testHanoi (n, a, b, c, moves) =
+  hanoi n a b c == moves
+
 ex6Tests :: [Test]
-ex6Tests = []
+ex6Tests =
+  let (a,b,c) = ("a","b","c") in
+  [
+    Test "testHanoi" testHanoi
+    [
+      (2,a,b,c, [(a,b), (a,c), (b,c)]),
+      (3,a,b,c,
+      [
+        (a,c), (a,b), (c,b),
+        (a,c),
+        (b,a), (b,c), (a,c)
+      ])
+    ]
+  ]
 
 -- All Tests ------------------------------------------
 
